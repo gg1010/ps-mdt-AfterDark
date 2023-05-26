6b9d49cc79a350b2a76075dff157836d99f66e5a
bb <55393468+gg1010@users.noreply.github.com>
2023-05-26 10:36:10 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -55,10 +55,8 @@ if Config.UseWolfknightRadar == true then
 			TriggerClientEvent('QBCore:Notify', src, 'EFTERLYST - HÆNDELSESID: '..incidentId..' | Ejer: '..owner..' | Nummerplade: '..plate, 'error', Config.WolfknightNotifyTime)
 		end
 
-		if Config.PlateScanForDriversLicense then
-			if driversLicense == false and vehicleOwner then
-				TriggerClientEvent('QBCore:Notify', src, 'NO DRIVERS LICENCE | Registered Owner: '..vehicleOwner..' | Plate: '..plate, 'error', Config.WolfknightNotifyTime)
-			end
+			if Config.PlateScanForDriversLicense and driversLicense == false and vehicleOwner then
+			TriggerClientEvent('QBCore:Notify', src, 'NO DRIVERS LICENCE | Registered Owner: '..vehicleOwner..' | Plate: '..plate, 'error', Config.WolfknightNotifyTime)
 		end
 
 
